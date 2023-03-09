fa71f32d4ef7c0dd04f852933d8c7ad8064850c6
wittdesigns <113542953+wittdesigns@users.noreply.github.com>
2023-03-09 09:32:48 -0500

    functional drag and drop

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let theButtons = document.querySelectorAll("#buttonHolder img"),
+    theHeading = document.querySelector("#headLine h1"),
+    puzzleBoard = document.querySelector(".puzzle-board"),
+    puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+	puzzlePiecesBoard = document.querySelectorAll(".puzzle-pieces"),
+    dropZones = document.querySelectorAll('.drop-zone'),
+    draggedPiece;
+
+	
+
+
+
+function handleStartDrag() {
+    console.log('started dragging this piece:', this);
+    draggedPiece = this;
+}
+
+function handleDragOver(e) {
+    e.preventDefault();
+    console.log('dragged over me');
+}
+
+
+function isDropZoneEmpty(zone) {
+    return !zone.classList.contains("occupied");
+}
+
+function handleDrop(e) {
+    e.preventDefault();
+    console.log('dropped something on me');
+    if (isDropZoneEmpty(this)) {
+        this.appendChild(draggedPiece);
+        this.classList.add("occupied");
+    } else {
+        console.log("This drop zone is already occupied");
+    }
+}
+
+function handleRemove(e) {
+    console.log('removed piece from drop zone');
+    this.classList.remove("occupied");
+}
+
+
+
+puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+
+dropZones.forEach(zone => {
+    zone.addEventListener("dragover", handleDragOver);
+    zone.addEventListener("drop", handleDrop);
+    zone.addEventListener("DOMNodeRemoved", handleRemove);
+});
+
+
+puzzlePiecesBoard.forEach(zone => {
+    zone.addEventListener("dragover", handleDragOver);
+    zone.addEventListener("drop", handleDrop);
+    zone.addEventListener("DOMNodeRemoved", handleRemove);
+});
+
+
+1
+
