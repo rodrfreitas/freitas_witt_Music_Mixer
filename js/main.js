4b980fc58b1a1c54d8b8f8e96df6d1f38732925e
wittdesigns <113542953+wittdesigns@users.noreply.github.com>
2023-03-17 09:47:19 -0400

    reel in out effect

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 let theButtons = document.querySelectorAll("#buttonHolder img"),
     theHeading = document.querySelector("#headLine h1"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
-	puzzlePiecesBoard = document.querySelectorAll(".puzzle-pieces"),
+    puzzlePiecesBoard = document.querySelectorAll(".puzzle-pieces"),
     dropZones = document.querySelectorAll('.drop-zone'),
     draggedPiece;
-
-	
-
 
 
 function handleStartDrag() {
@@ -19,53 +16,54 @@ function handleDragOver(e) {
     console.log('dragged over me');
 }
 
-
 function isDropZoneEmpty(zone) {
     return !zone.classList.contains("occupied");
 }
-
-
 
 function handleDrop(e) {
     e.preventDefault();
     console.log('dropped something on me');
     if (isDropZoneEmpty(this)) {
-      this.appendChild(draggedPiece);
-      this.classList.add("occupied");
-      const soundSrc = draggedPiece.getAttribute("data-audio");
-      const sound = new Audio(soundSrc);
-      sound.loop = true; 
-      sound.play();
+        this.appendChild(draggedPiece);
+        this.classList.add("occupied");
+        const soundSrc = draggedPiece.getAttribute("data-audio");
+        const sound = new Audio(soundSrc);
+        sound.loop = true;
+        sound.play();
     } else {
-      console.log("This drop zone is already occupied");
+        console.log("This drop zone is already occupied");
     }
-  }
-
-
-
-function handleRemove(e) {
-    console.log('removed piece from drop zone');
-    this.classList.remove("occupied");
 }
 
+function handleRemove(mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            if (!mutation.target.hasChildNodes()) {
+                console.log('removed piece from drop zone');
+                mutation.target.classList.remove("occupied");
+            }
+        }
+    }
+}
 
-
-puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+const observerConfig = {
+    childList: true
+};
 
 dropZones.forEach(zone => {
     zone.addEventListener("dragover", handleDragOver);
     zone.addEventListener("drop", handleDrop);
-    zone.addEventListener("DOMNodeRemoved", handleRemove);
-});
 
+    const observer = new MutationObserver(handleRemove);
+    observer.observe(zone, observerConfig);
+});
 
 puzzlePiecesBoard.forEach(zone => {
     zone.addEventListener("dragover", handleDragOver);
     zone.addEventListener("drop", handleDrop);
-    zone.addEventListener("DOMNodeRemoved", handleRemove);
+
+    const observer = new MutationObserver(handleRemove);
+    observer.observe(zone, observerConfig);
 });
 
-
-
-
-
+puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
